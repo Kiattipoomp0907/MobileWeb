3fde1b831bf1e45185e34e2f0288618cbebc0aca
Kiattipoom0907 <kiattipoom.s@kkumail.com>
2026-02-22 22:25:06 +0700

    replace new lab8

---
+++ apps/lab08-ai/src/core/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuiegZ_CNMW9Gt0BNBGDaRp7phVj19CQc",
+  authDomain: "mobileapp-3fc09.firebaseapp.com",
+  projectId: "mobileapp-3fc09",
+  storageBucket: "mobileapp-3fc09.firebasestorage.app",
+  messagingSenderId: "682665399312",
+  appId: "1:682665399312:web:dc0d553eaa805818ffa695",
+  measurementId: "G-7K6L5J3BDY"
+};
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
